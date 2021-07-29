6d41b282e61ea1d79e37e6d02dd57ecbc471a553
Selective031 <calle@lanstep.com>
2021-07-29 12:48:43 +0200

    pitch fix

--- RazorBoard/Core/Src/mpu6050.c
+++ RazorBoard/Core/Src/mpu6050.c
@@ -117,7 +117,8 @@ void MPU6050_Read_Accel(void) {
 	float r, p;
 
 	if (board_revision == 12) {
-        p = atan2(Ax , Ay) * 57.3 - 90;										// Ay, Az
+        p = atan2(Ax , Ay) * 57.3;									// Ay, Az
+    	p -= 90;
         r = atan2((- Az) , sqrtf(Ay * Ay + Ax * Ax)) * 57.3;			// Ax, Ay, Az, Az
 	} else {
         r = atan2(Ay , Az) * 57.3;										// Ay, Az
